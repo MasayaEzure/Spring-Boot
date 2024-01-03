87211148535afcfdb7625158d59c1640a255e965
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2024-01-03 23:47:52 +0900

    TeacherServiceの新規作成、作成済みファイルを一部編集

--- spring-mybatis/src/main/java/dev/itboot/mb/controller/TeacherController.java
+++ spring-mybatis/src/main/java/dev/itboot/mb/controller/TeacherController.java
@@ -2,20 +2,52 @@ package dev.itboot.mb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import dev.itboot.mb.repository.TeacherMapper;
+import dev.itboot.mb.model.Teacher;
+import dev.itboot.mb.service.TeacherService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
 public class TeacherController {
 
-	private final TeacherMapper mapper;
+	private final TeacherService service;
 
 	@GetMapping("/")
 	public String getAllTeachers(Model model) {
-		model.addAttribute("page", mapper.selectAll());
+		model.addAttribute("page", service.selectAll());
 		return "list";
+	}
+
+	@GetMapping("/add")
+	public String addTeacher(@ModelAttribute Teacher teacher) {
+		return "form";
+	}
+
+	@PostMapping("/process")
+	public String process(@Validated @ModelAttribute Teacher teacher,
+			BindingResult result) {
+		if (result.hasErrors()) return "form";
+		service.save(teacher);
+
+		return "redirect:/";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String editTeacher(@PathVariable Long id, Model model) {
+		model.addAttribute("teacher", service.selectByPrimaryKey(id));
+		return "form";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteTeacher(@PathVariable Long id) {
+		service.deleteByPrimaryKey(id);
+		return "redirect:/";
 	}
 }
--- spring-mybatis/src/main/java/dev/itboot/mb/repository/TeacherMapper.java
+++ spring-mybatis/src/main/java/dev/itboot/mb/repository/TeacherMapper.java
@@ -2,8 +2,11 @@ package dev.itboot.mb.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import dev.itboot.mb.model.Teacher;
 
@@ -12,4 +15,26 @@ public interface TeacherMapper {
 
 	@Select("SELECT * FROM teacher")
 	List<Teacher> selectAll();
+
+	@Select(""
+			+ "SELECT * FROM teacher "
+			+ "WHERE id = #{id}"
+			)
+	Teacher selectByPrimaryKey(Long id);
+
+	@Insert(""
+			+ "INSERT INTO teacher(user_name, email) "
+			+ "VALUES (#{userName}, #{email})"
+			)
+	int insert(Teacher record);
+
+	@Update(""
+			+ "UPDATE teacher SET "
+			+ "user_name = #{userName}, email = ${email} "
+			+ "WHERE id = #{id}"
+			)
+	int updateByPrimaryKey(Teacher record);
+
+	@Delete("DELETE * FROM teacher WHERE id = #{id}")
+	int deleteByPrimaryKey(Long id);
 }
