2489f22b39024d85d4f16cad62dc4067cb030b74
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2024-01-03 16:49:18 +0900

    各プロジェクト内に不要なファイルが含まれていたので一旦削除

--- spring-ioc-aop/src/main/java/com/example/demo/repository/CoffeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Coffee;
-
-public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
-
-}
--- spring-ioc-aop/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO coffee(id, name)
-VALUES(1, 'ブレンドコーヒー'), (2, 'ジャワコーヒー');
