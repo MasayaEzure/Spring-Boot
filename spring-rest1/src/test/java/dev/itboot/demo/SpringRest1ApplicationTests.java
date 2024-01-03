d95297b1de3791d9c5524b3451fec64530f3c84b
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2024-01-03 18:03:08 +0900

    spring-restXのソースを再プッシュ

---
+++ spring-rest1/src/test/java/dev/itboot/demo/SpringRest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.itboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
