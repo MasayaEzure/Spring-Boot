cf8805c1b72204ca774d42dfa6b43009bee3f039
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2024-01-03 15:07:50 +0900

    SpringRest3Applicationを一部編集

--- spring-rest3/src/main/java/dev/itboot/rest/SpringRest3Application.java
+++ spring-rest3/src/main/java/dev/itboot/rest/SpringRest3Application.java
@@ -3,6 +3,13 @@ package dev.itboot.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+@OpenAPIDefinition(info = @Info(
+		title       = "Task API",
+		version     = "1.0.0",
+		description = "Taskアプリケーションに関するAPIです"
+		))
 @SpringBootApplication
 public class SpringRest3Application {
 
