10e5d302794a0621cee0b5d3f28b33b8570ad0af
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2024-01-04 16:27:01 +0900

    SQLをXMLに記述するように変更

--- spring-mybatis/src/main/java/dev/itboot/mb/repository/TeacherMapper.java
+++ spring-mybatis/src/main/java/dev/itboot/mb/repository/TeacherMapper.java
@@ -2,39 +2,21 @@ package dev.itboot.mb.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import dev.itboot.mb.model.Teacher;
 
 @Mapper
 public interface TeacherMapper {
 
-	@Select("SELECT * FROM teacher")
+	// 各SQLはTeacherMapper.xmlに記述したため削除
 	List<Teacher> selectAll();
 
-	@Select(""
-			+ "SELECT * FROM teacher "
-			+ "WHERE id = #{id}"
-			)
 	Teacher selectByPrimaryKey(Long id);
 
-	@Insert(""
-			+ "INSERT INTO teacher(user_name, email) "
-			+ "VALUES (#{userName}, #{email})"
-			)
 	int insert(Teacher record);
 
-	@Update(""
-			+ "UPDATE teacher SET "
-			+ "user_name = #{userName}, email = #{email} "
-			+ "WHERE id = #{id}"
-			)
 	int updateByPrimaryKey(Teacher record);
 
-	@Delete("DELETE FROM teacher WHERE id = #{id}")
 	int deleteByPrimaryKey(Long id);
 }
