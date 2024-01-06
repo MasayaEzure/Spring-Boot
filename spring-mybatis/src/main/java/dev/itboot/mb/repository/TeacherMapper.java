8d9230b4e1ff7c48e64b5f6a8b5a8cd467dc6055
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2024-01-06 10:57:42 +0900

    PageHelperでSQLを動的に動かす方法で実装

--- spring-mybatis/src/main/java/dev/itboot/mb/repository/TeacherMapper.java
+++ spring-mybatis/src/main/java/dev/itboot/mb/repository/TeacherMapper.java
@@ -3,7 +3,8 @@ package dev.itboot.mb.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.domain.Pageable;
 
 import dev.itboot.mb.model.Teacher;
 
@@ -12,7 +13,8 @@ public interface TeacherMapper {
 	// 以下を追加
 	Long count();
 
-	List<Teacher> selectAll(RowBounds rowBounds);
+	// RowBoundsを使用しない方法で再実装
+	List<Teacher> selectAll(@Param("pageable") Pageable pageable);
 
 	// 各SQLはTeacherMapper.xmlに記述したため削除
 	List<Teacher> selectAll();
